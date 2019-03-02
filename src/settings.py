6ffbdc0ba9cadd1dfad1e8b566063c62fb38b763
Yuan Lu <yuan@ek12.com>
2019-03-02 10:47:54 -0800

    change all_labels from Set to dictionary

--- src/settings.py
+++ src/settings.py
@@ -3,11 +3,13 @@ Description: This file defines global constants/variables for all scripts/softwa
 Author: Yuan
 '''
 from __future__ import print_function
-from sets import Set
+#from sets import Set
 
 # Sets
-all_labels = {'hungry', 'happy', 'upset', 'annoy', 'angry', 'complain', 'comfortable', 'pain', 'attention', 
-              'beg', 'sick', 'scared'}
+all_labels = {'hungry':0, 'happy':1, 'upset':2, 'annoy':3, 'angry':4, 'complain':5, 'comfortable':6, 'pain':7, 'attention':8, 
+              'beg':9, 'sick':10, 'scared':11}
 
 # tuples
 all_formats = ('.wav', '.mp3', '.aiff', '.aif', '.au', '.flac', '.m4a', '.mp4', '.webm', '.mkv')
+
+data_dir = '/hdd/mlrom/Data/animal_voice'
